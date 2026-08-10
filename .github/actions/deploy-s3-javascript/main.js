4188dbed01fed98d3525ed3f8b69cbb7dea7a529
vishalkoshti18 <vishalkoshti18@gmail.com>
2026-08-10 18:41:01 +0530

    files in a dist folder will upload into the s3 bucket

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,16 @@ const exec = require('@actions/exec')
 const github = require('@actions/github')
 
 function run() {
+  //1) Get some information related to the bucket
+  const bucket = core.getInput('bucket', { required: true })
+  const bucketRegion = core.getInput('bucket-region', { required: true })
+  const distFolder = core.getInput('dist-folder', { required: true })
   core.notice('Hello from My custom Javascript Action');
+
+  // 2) Upload files
+  const s3Uri = `s3://${bucket}`
+  exec.exec(`aws s3 sync ${distFolder}} ${s3Uri} --region ${bucketRegion}`);
 }
 
 run();
+
