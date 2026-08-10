9a30af0db33a19bf8b4171f345140571671adfc3
vishalkoshti18 <vishalkoshti18@gmail.com>
2026-08-10 19:03:57 +0530

    added output URL

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,6 +12,10 @@ function run() {
   // 2) Upload files
   const s3Uri = `s3://${bucket}`
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  // 3) get URL
+  const websiteUrl = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com/`
+  core.setOutput('website-url',websiteUrl)
 }
 
 run();
