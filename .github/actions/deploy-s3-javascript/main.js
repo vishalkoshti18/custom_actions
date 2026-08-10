75aac23e9df12cf0229bc0af5e2f591b84a7a114
vishalkoshti18 <vishalkoshti18@gmail.com>
2026-08-10 18:44:54 +0530

    fixed syntax issue

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -11,7 +11,7 @@ function run() {
 
   // 2) Upload files
   const s3Uri = `s3://${bucket}`
-  exec.exec(`aws s3 sync ${distFolder}} ${s3Uri} --region ${bucketRegion}`);
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 }
 
 run();
