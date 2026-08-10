0ee7f25804fda142bc69d9d3aac5cbc547dc7604
vishalkoshti18 <vishalkoshti18@gmail.com>
2026-08-10 19:10:39 +0530

    removed trailing forwardspace

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -14,7 +14,7 @@ function run() {
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
   // 3) get URL
-  const websiteUrl = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com/`
+  const websiteUrl = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`
   core.setOutput('website-url',websiteUrl)
 }
 
